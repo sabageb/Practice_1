2c11c09dc17293de3358d5e15815252dea7c9e3b
Saba <sabageb@gmail.com>
2021-04-24 21:13:22 -0700

    adding thank you after publishing to github

--- src/day1/helloworld.java
+++ src/day1/helloworld.java
@@ -3,6 +3,11 @@ package day1;
 public class helloworld {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("Adding GitHub now");
+        System.out.println("Yay stuff are on GITHUB");
+        System.out.println("thank you!");
+        // adding final wrap uo code
+
 
     }
 }
